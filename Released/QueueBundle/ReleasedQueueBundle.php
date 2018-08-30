17d1809858e6c246c904b39fb9ff51e7e3949a11
Sergey Ryabenko <sergey@mobillogix.com>
2018-08-30 03:43:57 +0300

    S4 support

--- Released/QueueBundle/ReleasedQueueBundle.php
+++ Released/QueueBundle/ReleasedQueueBundle.php
@@ -2,7 +2,9 @@
 
 namespace Released\QueueBundle;
 
+use Released\QueueBundle\Command as Cmd;
 use Released\QueueBundle\DependencyInjection\Pass\QueueServicePass;
+use Symfony\Component\Console\Application;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -17,4 +19,12 @@ class ReleasedQueueBundle extends Bundle
         $container->addCompilerPass(new QueueServicePass());
     }
 
+    /** {@inheritdoc} */
+    public function registerCommands(Application $application)
+    {
+        $application->add(new Cmd\ReleasedQueueGcCommand());
+        $application->add(new Cmd\ReleasedQueueExecuteTaskAmqpCommand());
+        $application->add(new Cmd\ReleasedQueueCreateTaskCommand());
+        $application->add(new Cmd\ReleasedQueueExecuteTaskDbCommand());
+    }
 }
