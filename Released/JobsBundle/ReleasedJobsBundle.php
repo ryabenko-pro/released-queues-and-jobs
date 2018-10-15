89b49f12cb35e3501dd9913b67209d4e0bd5fed3
Sergey Ryabenko <sergey.ryabenko@cleoo.com>
2018-10-15 21:03:06 +0300

    Fixes

--- Released/JobsBundle/ReleasedJobsBundle.php
+++ Released/JobsBundle/ReleasedJobsBundle.php
@@ -2,8 +2,22 @@
 
 namespace Released\JobsBundle;
 
+use Released\JobsBundle\Command as Cmd;
+use Symfony\Component\Console\Application;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class ReleasedJobsBundle extends Bundle
 {
+    public function registerCommands(Application $application)
+    {
+        $application->add(new Cmd\ReleasedJobsCreateCommand());
+        $application->add(new Cmd\ReleasedJobsDevFullCommand());
+        $application->add(new Cmd\ReleasedJobsExecuteCommand());
+        $application->add(new Cmd\ReleasedJobsFinishCommand());
+        $application->add(new Cmd\ReleasedJobsPackageRunCommand());
+        $application->add(new Cmd\ReleasedJobsPlanCommand());
+        $application->add(new Cmd\ReleasedJobsStartCommand());
+        $application->add(new Cmd\ReleasedJobsStopCommand());
+    }
+
 }
